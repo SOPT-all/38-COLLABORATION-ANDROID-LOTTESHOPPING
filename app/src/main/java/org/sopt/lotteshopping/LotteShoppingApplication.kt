9a2261abbbf39453f10c99f1c48280d6c6a9b6a9
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-10 01:15:27 +0900

    [add/#1] 초기 application 세팅

---
+++ app/src/main/java/org/sopt/lotteshopping/LotteShoppingApplication.kt
@@ -0,0 +1,19 @@
+package org.sopt.lotteshopping
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class LotteShoppingApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
