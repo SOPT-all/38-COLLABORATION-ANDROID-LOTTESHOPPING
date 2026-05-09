1aa40057f0621890e471a334e9533ea352e5d33a
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-10 01:17:31 +0900

    [add/#1] DummyDatasource 추가

---
+++ app/src/main/java/org/sopt/lotteshopping/data/remote/datasourceImpl/DummyDataSourceImpl.kt
@@ -0,0 +1,11 @@
+package org.sopt.lotteshopping.data.remote.datasourceImpl
+import org.sopt.lotteshopping.data.remote.datasource.DummyDataSource
+import org.sopt.lotteshopping.data.remote.dto.DummyResponseDto
+import org.sopt.lotteshopping.data.remote.service.DummyService
+import javax.inject.Inject
+
+class DummyDataSourceImpl @Inject constructor(
+    private val service: DummyService,
+) : DummyDataSource {
+    override suspend fun getDummy(): DummyResponseDto = service.getDummy()
+}
