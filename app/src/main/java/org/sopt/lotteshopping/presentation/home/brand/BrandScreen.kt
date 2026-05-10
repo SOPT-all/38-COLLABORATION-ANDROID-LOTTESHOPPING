f3a8a81174b620dc96378bd2d864b2753a4f9a59
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-10 11:49:21 +0900

    [chore/#1] 패키지 구조 이동

--- app/src/main/java/org/sopt/lotteshopping/presentation/home/brand/BrandScreen.kt
+++ app/src/main/java/org/sopt/lotteshopping/presentation/home/brand/BrandScreen.kt
@@ -1,4 +1,4 @@
-package org.sopt.lotteshopping.presentation.brand
+package org.sopt.lotteshopping.presentation.home.brand
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
