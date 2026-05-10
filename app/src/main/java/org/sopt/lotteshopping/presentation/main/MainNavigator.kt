16c4f04831d297e9de7fa69ef681f300a512a475
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-10 15:58:32 +0900

    [del/#1] 사용안하는 import 제거

--- app/src/main/java/org/sopt/lotteshopping/presentation/main/MainNavigator.kt
+++ app/src/main/java/org/sopt/lotteshopping/presentation/main/MainNavigator.kt
@@ -2,7 +2,6 @@ package org.sopt.lotteshopping.presentation.main
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
