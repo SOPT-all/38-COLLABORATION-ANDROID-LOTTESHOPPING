b53801063e52a6d9b4077639d564bb33b3823cf3
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-10 11:49:00 +0900

    [feat/#3] MainNavigator 구현

---
+++ app/src/main/java/org/sopt/lotteshopping/presentation/main/MainNavigator.kt
@@ -0,0 +1,75 @@
+package org.sopt.lotteshopping.presentation.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import org.sopt.lotteshopping.presentation.home.navigation.Home
+import org.sopt.lotteshopping.presentation.home.navigation.navigateToHome
+import org.sopt.lotteshopping.presentation.main.component.MainTab
+import org.sopt.lotteshopping.presentation.shopping.navigation.navigateToShopping
+
+class MainNavigator(
+    val navController: NavHostController,
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() =
+            navController
+                .currentBackStackEntryAsState()
+                .value
+                ?.destination
+
+    val startDestination = Home
+
+    val currentTab: MainTab?
+        @Composable get() {
+            val destination = currentDestination
+            return MainTab.entries.find { tab ->
+                destination?.hasRoute(tab.route::class) == true
+            }
+        }
+
+    fun navigate(tab: MainTab) {
+        val navOptions =
+            navOptions {
+                navController.currentDestination?.route?.let {
+                    popUpTo(it) {
+                        saveState = true
+                        inclusive = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+
+        when (tab) {
+            MainTab.HOME -> navController.navigateToHome(navOptions = navOptions)
+            MainTab.PLACE -> {} // TODO: 디자인 미구현 사항
+            MainTab.PAY -> {} // TODO: 디자인 미구현 사항
+            MainTab.SHOPPING -> navController.navigateToShopping(navOptions = navOptions)
+            MainTab.MY -> {} // TODO: 디자인 미구현 사항
+        }
+    }
+
+    fun navigateUp() = navController.navigateUp()
+
+    @Composable
+    fun showBottomBar(): Boolean {
+        val destination = currentDestination
+        return MainTab.contains { route ->
+            destination?.hasRoute(route::class) == true
+        }
+    }
+}
+
+@Composable
+fun rememberMainNavigator(
+    navController: NavHostController = rememberNavController(),
+): MainNavigator =
+    remember(navController) {
+        MainNavigator(navController)
+    }
