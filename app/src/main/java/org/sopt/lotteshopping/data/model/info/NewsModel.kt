96a804909b2fa4af579e8897b7a2262832ca76b0
dodo <eg4rang@naver.com>
2026-05-11 21:42:19 +0900

    [feat/#7] shopping card product 구현

---
+++ app/src/main/java/org/sopt/lotteshopping/data/model/info/NewsModel.kt
@@ -0,0 +1,12 @@
+package org.sopt.lotteshopping.data.model.info
+
+data class NewsModel (
+    val id: Long,
+    val title: String,
+    val targetBranch: String,
+    val startDate: String,
+    val endDate: String,
+    val imageUrl: String,
+) {
+    val hasTargetBranch = targetBranch.isNotBlank()
+}
