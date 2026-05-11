5463f5c03ba7e43cf43c2a2e7bb01a0284be9593
lemonsoda1 <angrybird2600@gmail.com>
2026-05-11 11:09:29 +0900

    [add/#5] 타이포그래피 em 단위 적용

--- app/src/main/java/org/sopt/lotteshopping/core/designsystem/theme/Typography.kt
+++ app/src/main/java/org/sopt/lotteshopping/core/designsystem/theme/Typography.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import org.sopt.lotteshopping.R
 
@@ -63,88 +64,75 @@ val defaultLotteTypography = LotteTypography(
         b30 = lotteTextStyle(
             weight = FontWeight.Bold,
             size = 30,
-            lineHeight = 42f,
-            letterSpacing = -0.3f,
+            letterSpacing = DISPLAY_LETTER_SPACING,
         ),
     ),
     title = LotteTitleTypography(
         sb18 = lotteTextStyle(
             weight = FontWeight.SemiBold,
             size = 18,
-            lineHeight = 25.2f,
-            letterSpacing = -0.18f,
+            letterSpacing = TITLE_LETTER_SPACING,
         ),
         m18 = lotteTextStyle(
             weight = FontWeight.Medium,
             size = 18,
-            lineHeight = 25.2f,
-            letterSpacing = -0.18f,
+            letterSpacing = TITLE_LETTER_SPACING,
         ),
         r18 = lotteTextStyle(
             weight = FontWeight.Normal,
             size = 18,
-            lineHeight = 25.2f,
-            letterSpacing = -0.18f,
+            letterSpacing = TITLE_LETTER_SPACING,
         ),
     ),
     body = LotteBodyTypography(
         sb16 = lotteTextStyle(
             weight = FontWeight.SemiBold,
             size = 16,
-            lineHeight = 22.4f,
-            letterSpacing = -0.32f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
         m16 = lotteTextStyle(
             weight = FontWeight.Medium,
             size = 16,
-            lineHeight = 22.4f,
-            letterSpacing = -0.32f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
         r16 = lotteTextStyle(
             weight = FontWeight.Normal,
             size = 16,
-            lineHeight = 22.4f,
-            letterSpacing = -0.32f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
         sb14 = lotteTextStyle(
             weight = FontWeight.SemiBold,
             size = 14,
-            lineHeight = 19.6f,
-            letterSpacing = -0.28f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
         m14 = lotteTextStyle(
             weight = FontWeight.Medium,
             size = 14,
-            lineHeight = 19.6f,
-            letterSpacing = -0.28f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
         r14 = lotteTextStyle(
             weight = FontWeight.Normal,
             size = 14,
-            lineHeight = 19.6f,
-            letterSpacing = -0.28f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
     ),
     label = LotteLabelTypography(
         m12 = lotteTextStyle(
             weight = FontWeight.Medium,
             size = 12,
-            lineHeight = 16.8f,
-            letterSpacing = -0.24f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
         r12 = lotteTextStyle(
             weight = FontWeight.Normal,
             size = 12,
-            lineHeight = 16.8f,
-            letterSpacing = -0.24f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
     ),
     caption = LotteCaptionTypography(
         r10 = lotteTextStyle(
             weight = FontWeight.Normal,
             size = 10,
-            lineHeight = 14f,
-            letterSpacing = -0.2f,
+            letterSpacing = BODY_LETTER_SPACING,
         ),
     ),
 )
@@ -161,12 +149,16 @@ val defaultMaterialTypography = Typography(
 private fun lotteTextStyle(
     weight: FontWeight,
     size: Int,
-    lineHeight: Float,
     letterSpacing: Float,
 ) = TextStyle(
     fontFamily = NotoSansKr,
     fontWeight = weight,
     fontSize = size.sp,
-    lineHeight = lineHeight.sp,
-    letterSpacing = letterSpacing.sp,
+    lineHeight = DEFAULT_LINE_HEIGHT,
+    letterSpacing = letterSpacing.em,
 )
+
+private val DEFAULT_LINE_HEIGHT = 1.4.em
+private const val DISPLAY_LETTER_SPACING = -0.01f
+private const val TITLE_LETTER_SPACING = -0.01f
+private const val BODY_LETTER_SPACING = -0.02f
