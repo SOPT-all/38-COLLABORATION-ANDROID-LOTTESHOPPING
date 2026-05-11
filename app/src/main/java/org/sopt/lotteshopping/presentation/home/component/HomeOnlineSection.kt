f78cccc5e4fb4a24f673f827c29ca627bfcf4513
yerim0829 <123yellly@gmail.com>
2026-05-12 04:17:37 +0900

    [feat/#10] HomeOnlineSection 구현

---
+++ app/src/main/java/org/sopt/lotteshopping/presentation/home/component/HomeOnlineSection.kt
@@ -0,0 +1,85 @@
+package org.sopt.lotteshopping.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.lotteshopping.R
+import org.sopt.lotteshopping.data.onlineMallItems
+
+
+@Composable
+fun HomeOnlineSection() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 14.dp)
+                .padding(vertical = 10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+
+        ) {
+            Text(text = "온라인몰",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.ExtraBold)
+
+            Icon (
+                painter = painterResource(id = R.drawable.ic_chevron_right),
+                contentDescription = null,
+                modifier = Modifier.padding(end = 20.dp)
+            )
+
+        }
+
+        HorizontalDivider(
+            color = Color(0xFFECEDED),
+            thickness = 1.dp
+        )
+
+        LazyRow(
+            contentPadding = PaddingValues(horizontal = 14.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+
+        ) {
+
+            items(onlineMallItems) { item ->
+                HomeOnlineCard(
+                    imageRes = item.imageRes,
+                    title = item.title
+                )
+            }
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeOnlineSectionPreview() {
+    HomeOnlineSection()
+}
