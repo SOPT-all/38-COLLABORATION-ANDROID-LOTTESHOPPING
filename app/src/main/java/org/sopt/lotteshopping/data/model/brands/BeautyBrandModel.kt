3264a13cb5eecd0549e9462c4dbd7ab92b00955f
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-12 16:33:52 +0900

    [mod/#8] TODO 추가

--- app/src/main/java/org/sopt/lotteshopping/data/model/brands/BeautyBrandModel.kt
+++ app/src/main/java/org/sopt/lotteshopping/data/model/brands/BeautyBrandModel.kt
@@ -1,7 +1,8 @@
 package org.sopt.lotteshopping.data.model.brands
 
+// TODO (유빈) : api 필드 서버 명세에 따라 변경
 data class BeautyBrandModel(
     val id: Long,
     val name: String,
-    val imageUrl: String
+    val imageUrl: String,
 )
