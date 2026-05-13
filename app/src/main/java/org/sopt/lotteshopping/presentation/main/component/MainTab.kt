d620946ebd7e1c5ac436e496d852b9245b13c0da
yerim0829 <123yellly@gmail.com>
2026-05-14 02:40:28 +0900

    [mod/#10] titleRes 수정

--- app/src/main/java/org/sopt/lotteshopping/presentation/main/component/MainTab.kt
+++ app/src/main/java/org/sopt/lotteshopping/presentation/main/component/MainTab.kt
@@ -1,59 +1,49 @@
 package org.sopt.lotteshopping.presentation.main.component
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import org.sopt.lotteshopping.R
 import org.sopt.lotteshopping.core.common.naviagtion.MainTabRoute
 import org.sopt.lotteshopping.presentation.home.navigation.Home
 import org.sopt.lotteshopping.presentation.my.navigation.My
 import org.sopt.lotteshopping.presentation.pay.Pay
 import org.sopt.lotteshopping.presentation.place.Place
-import org.sopt.lotteshopping.presentation.shopping.navigation.Shopping
 
 
 enum class MainTab(
     @get:DrawableRes val selectedIconRes: Int,
     @get:DrawableRes val unselectedIconRes: Int,
-    val title: String,
+    @get:StringRes val titleRes: Int,
     val route: MainTabRoute,
 ) {
     HOME(
         selectedIconRes = R.drawable.ic_nav_home_selected,
         unselectedIconRes = R.drawable.ic_nav_home_unselected,
-        title = "홈",
+        titleRes= R.string.home,
         route = Home,
     ),
     PLACE(
         selectedIconRes = R.drawable.ic_nav_place_selected,
         unselectedIconRes = R.drawable.ic_nav_place_unselected,
-        title = "지점안내",
-        route = Place,
+        titleRes= R.string.place,
+        route= Place,
     ),
     PAY(
         selectedIconRes = R.drawable.ic_nav_pay_selected,
         unselectedIconRes = R.drawable.ic_nav_pay_unselected,
-        title = "",
-        route = Pay,
+        titleRes=R.string.pay,
+        route= Pay,
     ),
     SHOPPING(
         selectedIconRes = R.drawable.ic_nav_shopping_selected,
         unselectedIconRes = R.drawable.ic_nav_shopping_unselected,
-        title = "쇼핑정보",
-        route = Shopping,
+        titleRes= R.string.shopping,
+        route= Pay,
     ),
     MY(
         selectedIconRes = R.drawable.ic_nav_my_selected,
         unselectedIconRes = R.drawable.ic_nav_my_unselected,
-        title = "MY",
-        route = My,
+        titleRes = R.string.my,
+        route= My
     );
-
-    companion object {
-        fun find(predicate: (MainTabRoute) -> Boolean): MainTab? {
-            return entries.find { predicate(it.route) }
-        }
-
-        fun contains(predicate: (MainTabRoute) -> Boolean): Boolean {
-            return entries.any { predicate(it.route) }
-        }
-    }
 }
