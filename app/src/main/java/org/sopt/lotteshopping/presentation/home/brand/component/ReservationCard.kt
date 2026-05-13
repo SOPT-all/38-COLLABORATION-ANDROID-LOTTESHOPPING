f76937c8cf5534f3651d75375c389a63032f23e4
lemonsoda1 <angrybird2600@gmail.com>
2026-05-14 02:55:18 +0900

    [fix/#11] ReservationCard 레이아웃 정책 수정

--- app/src/main/java/org/sopt/lotteshopping/presentation/home/brand/component/ReservationCard.kt
+++ app/src/main/java/org/sopt/lotteshopping/presentation/home/brand/component/ReservationCard.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.lotteshopping.R
@@ -35,8 +32,7 @@ fun ReservationCard(
 ) {
     Row(
         modifier = modifier
-            .width(320.dp)
-            .height(134.dp)
+            .fillMaxWidth()
             .clip(RoundedCornerShape(12.dp))
             .background(LotteTheme.colors.gray50)
             .padding(
@@ -61,15 +57,11 @@ fun ReservationCard(
                     text = title,
                     color = LotteTheme.colors.black,
                     style = LotteTheme.typography.label.m12,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
                 )
                 Text(
                     text = period,
                     color = LotteTheme.colors.black,
                     style = LotteTheme.typography.caption.r10,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
                 )
             }
 
