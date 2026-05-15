ada612a9b6e0b936095be3c38c05d7224bfce3ff
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-16 01:23:48 +0900

    [feat/#19]  HomeUiState 정의

--- app/src/main/java/org/sopt/lotteshopping/presentation/home/HomeUiState.kt
+++ app/src/main/java/org/sopt/lotteshopping/presentation/home/HomeUiState.kt
@@ -1,16 +1,20 @@
 package org.sopt.lotteshopping.presentation.home
 
 import org.sopt.lotteshopping.R
+import org.sopt.lotteshopping.data.model.banners.HomeBottomBannerModel
 import org.sopt.lotteshopping.data.model.banners.HomeTopBannerModel
 import org.sopt.lotteshopping.data.model.brands.BeautyBrandModel
+import org.sopt.lotteshopping.data.model.preferences.HomePreferenceModel
 import org.sopt.lotteshopping.presentation.home.component.HomeStoreTab
 import org.sopt.lotteshopping.presentation.home.component.HomeTabType
 import org.sopt.lotteshopping.presentation.home.model.BeautyContentModel
+import org.sopt.lotteshopping.presentation.home.model.HomeOnlineItemModel
+import kotlin.collections.listOf
 
 data class HomeUiState(
     val selectedHomeTab: HomeTabType = HomeTabType.DEPARTMENT_STORE,
     val selectedStoreTab: HomeStoreTab = HomeStoreTab.DEPARTMENT_STORE,
-    val banners: List<HomeTopBannerModel> = emptyList(),
+    val topBanners: List<HomeTopBannerModel> = emptyList(),
     val brands: List<BeautyBrandModel> = emptyList(),
     val contents: List<BeautyContentModel> = listOf(
         BeautyContentModel(R.drawable.img_home_collection_aesop, "[Aesop]", "감각적인 데일리 케어"),
@@ -18,4 +22,12 @@ data class HomeUiState(
         BeautyContentModel(R.drawable.img_home_collection_kiehls, "[Kiehl's]", "가정의달 프로모션"),
         BeautyContentModel(R.drawable.img_home_collection_sk2, "[SK-II]", "FESTA 특별 이벤트")
     ),
+    val preference: List<HomePreferenceModel> = emptyList(),
+    val onlineMall: List<HomeOnlineItemModel> = listOf(
+        HomeOnlineItemModel(imageRes = R.drawable.img_home_onlinemall1, label = "[탬버린즈]", title = "탬버린즈 브랜드 데이"),
+        HomeOnlineItemModel(imageRes = R.drawable.img_home_onlinemall2, label = "[Dyson]", title = "공식스토어 리뉴얼 오픈"),
+        HomeOnlineItemModel(imageRes = R.drawable.img_home_onlinemall3, label = "[프리 썸머 위크]", title = "여름 신상품 모음")
+    ),
+    val bottomBanner: HomeBottomBannerModel? = null,
 )
+
