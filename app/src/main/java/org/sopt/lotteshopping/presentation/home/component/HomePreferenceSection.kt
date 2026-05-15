ae9f2630dec2c362cf26468d852b636b94593a23
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-15 19:33:46 +0900

    [refactor/#10] HomePreferenceSectrion 코드 수정

--- app/src/main/java/org/sopt/lotteshopping/presentation/home/component/HomePreferenceSection.kt
+++ app/src/main/java/org/sopt/lotteshopping/presentation/home/component/HomePreferenceSection.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -18,9 +17,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.sopt.lotteshopping.R
 import org.sopt.lotteshopping.core.designsystem.component.UrlImage
+import org.sopt.lotteshopping.core.designsystem.theme.LOTTESHOPPINGTheme
 import org.sopt.lotteshopping.core.designsystem.theme.LotteTheme
-import org.sopt.lotteshopping.data.HomePreferenceModel
+import org.sopt.lotteshopping.data.model.preferences.HomePreferenceModel
 
 
 @Composable
@@ -32,11 +33,10 @@ fun HomePreferenceSection(
         modifier = modifier
             .fillMaxWidth()
             .background(LotteTheme.colors.black)
-    )
-    {
+            .padding(top = 24.dp, bottom = 20.dp),
+    ) {
         Text(
-            modifier = Modifier
-                .padding(top = 24.dp, start = 14.dp),
+            modifier = Modifier.padding(start = 14.dp),
             text = "취향 확장",
             color = LotteTheme.colors.white,
             style = LotteTheme.typography.title.sb18
@@ -45,8 +45,7 @@ fun HomePreferenceSection(
         Spacer(modifier = Modifier.height(4.dp))
 
         Text(
-            modifier = Modifier
-                .padding(start = 14.dp),
+            modifier = Modifier.padding(start = 14.dp),
             text = "나의 세계를 넓히는 감각적인 제안",
             color = LotteTheme.colors.gray300,
             style = LotteTheme.typography.body.r14
@@ -58,50 +57,40 @@ fun HomePreferenceSection(
             horizontalArrangement = Arrangement.spacedBy(10.dp),
             contentPadding = PaddingValues(horizontal = 14.dp)
         ) {
-
-            items(preferenceItems) { item ->
-                HomePreferenceCard(
-                    imageUrl = item.imageUrl,
-                    title = item.title,
-                    targetBranch = item.targetBranch,
-                    startDate = item.startDate,
-                    endDate = item.endDate
+            items(preferenceItems) { preferenceItem ->
+                HomePreferenceCardItem(
+                    preference = preferenceItem,
                 )
             }
         }
     }
-
 }
 
+
 @Composable
-private fun HomePreferenceCard(
-    imageUrl: String,
-    title: String,
-    targetBranch: String,
-    startDate: String,
-    endDate: String,
+private fun HomePreferenceCardItem(
+    preference: HomePreferenceModel,
     modifier: Modifier = Modifier
 ) {
 
     Column(
         modifier = modifier
-            .width(240.dp)
             .background(LotteTheme.colors.white)
+            .padding(horizontal = 14.dp)
             .padding(top = 14.dp, bottom = 24.dp)
-            .padding(horizontal=14.dp)
-    )
-    {
+    ) {
         UrlImage(
-            url = imageUrl,
+            url = preference.imageUrl,
             contentDescription = null,
-            modifier = Modifier
-                .size(212.dp),
+            modifier = Modifier.size(212.dp),
+            placeholderDrawable = R.drawable.ic_launcher_background,
             contentScale = ContentScale.Crop
         )
+
         Spacer(modifier = Modifier.height(20.dp))
 
         Text(
-            text = title,
+            text = preference.title,
             style = LotteTheme.typography.body.m14,
             color = LotteTheme.colors.black
         )
@@ -110,7 +99,7 @@ private fun HomePreferenceCard(
 
         Text(
             modifier = Modifier,
-            text = targetBranch,
+            text = preference.targetBranch,
             style = LotteTheme.typography.label.r12,
             color = LotteTheme.colors.gray400
         )
@@ -118,7 +107,7 @@ private fun HomePreferenceCard(
         Spacer(modifier = Modifier.height(2.dp))
 
         Text(
-            text = "$startDate ~ $endDate",
+            text = "${preference.startDate} ~ ${preference.endDate}",
             style = LotteTheme.typography.label.r12,
             color = LotteTheme.colors.gray400
         )
@@ -129,5 +118,26 @@ private fun HomePreferenceCard(
 @Preview(showBackground = true)
 @Composable
 private fun HomePreferenceSectionPreview() {
-    HomePreferenceSection(preferenceItems = emptyList())
+    LOTTESHOPPINGTheme {
+        HomePreferenceSection(
+            preferenceItems = listOf(
+                HomePreferenceModel(
+                    imageUrl = "",
+                    title = "[브랜드 읽기] Chanel의 No.5",
+                    targetBranch = "전점",
+                    startDate = "4.1(수)",
+                    endDate = "12.31(동)",
+                    id = 0,
+                ),
+                HomePreferenceModel(
+                    imageUrl = "",
+                    title = "[스테디셀러] Clinique 기획전",
+                    targetBranch = "전점",
+                    startDate = "4.1(수)",
+                    endDate = "12.31(목)",
+                    id = 1,
+                ),
+            )
+        )
+    }
 }
