7d1341ae6e24e38077e708cdbcf76dbd39d84607
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-16 01:24:26 +0900

    [feat/#19] HomeScreen 하단 구현

--- app/src/main/java/org/sopt/lotteshopping/presentation/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/lotteshopping/presentation/home/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -20,13 +21,18 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.sopt.lotteshopping.R
+import org.sopt.lotteshopping.core.designsystem.component.UrlImage
 import org.sopt.lotteshopping.core.designsystem.theme.LOTTESHOPPINGTheme
 import org.sopt.lotteshopping.core.designsystem.theme.LotteTheme.colors
+import org.sopt.lotteshopping.data.model.banners.HomeBottomBannerModel
 import org.sopt.lotteshopping.data.model.banners.HomeTopBannerModel
 import org.sopt.lotteshopping.data.model.brands.BeautyBrandModel
+import org.sopt.lotteshopping.data.model.preferences.HomePreferenceModel
 import org.sopt.lotteshopping.presentation.home.component.HomeBeautyBrandSection
 import org.sopt.lotteshopping.presentation.home.component.HomeBeautyContentSection
 import org.sopt.lotteshopping.presentation.home.component.HomeLocationHeader
+import org.sopt.lotteshopping.presentation.home.component.HomeOnlineSection
+import org.sopt.lotteshopping.presentation.home.component.HomePreferenceSection
 import org.sopt.lotteshopping.presentation.home.component.HomeServiceSection
 import org.sopt.lotteshopping.presentation.home.component.HomeStoreTab
 import org.sopt.lotteshopping.presentation.home.component.HomeTabType
@@ -83,10 +89,9 @@ private fun HomeScreen(
                 .padding(innerPadding)
         ) {
             item { HomeLocationHeader() }
+
             item {
-                HomeTopBanner(
-                    banners = uiState.banners,
-                )
+                HomeTopBanner(banners = uiState.topBanners)
                 Spacer(modifier = Modifier.height(20.dp))
             }
 
@@ -105,12 +110,14 @@ private fun HomeScreen(
                     modifier = Modifier.padding(horizontal = 20.dp)
                 )
             }
+
             item {
                 HomeServiceSection(
                     modifier = Modifier.padding(horizontal = 20.dp)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
             }
+
             item {
                 HomeBeautyBrandSection(
                     brands = uiState.brands,
@@ -118,6 +125,7 @@ private fun HomeScreen(
                 )
                 Spacer(modifier = Modifier.height(16.dp))
             }
+
             item {
                 Image(
                     painter = painterResource(id = R.drawable.img_home_banner_ad),
@@ -126,6 +134,37 @@ private fun HomeScreen(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 20.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            item {
+                HomePreferenceSection(
+                    preferenceItems = uiState.preference,
+                    modifier = Modifier.padding(horizontal = 20.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            item {
+                HomeOnlineSection(
+                    onlineItems = uiState.onlineMall,
+                    modifier = Modifier.padding(horizontal = 20.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            item {
+                UrlImage(
+                    url = uiState.bottomBanner?.imageUrl ?: "",
+                    contentDescription = null,
+                    placeholderDrawable = R.drawable.ic_launcher_background,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .width(320.dp)
+                        .height(98.dp)
                 )
             }
         }
@@ -138,7 +177,7 @@ private fun HomeScreenPreview() {
     LOTTESHOPPINGTheme {
         HomeScreen(
             uiState = HomeUiState(
-                banners = listOf(
+                topBanners = listOf(
                     HomeTopBannerModel(1L, 1, ""),
                     HomeTopBannerModel(2L, 2, ""),
                     HomeTopBannerModel(3L, 3, "")
@@ -148,7 +187,26 @@ private fun HomeScreenPreview() {
                     BeautyBrandModel(2L, "디올", "")
                 ),
                 selectedHomeTab = HomeTabType.DEPARTMENT_STORE,
-                selectedStoreTab = HomeStoreTab.DEPARTMENT_STORE
+                selectedStoreTab = HomeStoreTab.DEPARTMENT_STORE,
+                preference = listOf(
+                    HomePreferenceModel(
+                        imageUrl = "",
+                        title = "[브랜드 읽기] Chanel의 No.5",
+                        targetBranch = "전점",
+                        startDate = "4.1(수)",
+                        endDate = "12.31(동)",
+                        id = 0,
+                    ),
+                    HomePreferenceModel(
+                        imageUrl = "",
+                        title = "[스테디셀러] Clinique 기획전",
+                        targetBranch = "전점",
+                        startDate = "4.1(수)",
+                        endDate = "12.31(목)",
+                        id = 1,
+                    )
+                ),
+                bottomBanner = HomeBottomBannerModel(1L, "")
             ),
             onBrandClick = {},
             onHomeTabClick = {},
