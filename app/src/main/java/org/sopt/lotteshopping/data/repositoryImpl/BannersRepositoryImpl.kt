98a15f9081714d6f2a360d1cc10ed7d9ced599bd
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-18 15:54:05 +0900

    [mod/#27] suspendRunCatching 처리

--- app/src/main/java/org/sopt/lotteshopping/data/repositoryImpl/BannersRepositoryImpl.kt
+++ app/src/main/java/org/sopt/lotteshopping/data/repositoryImpl/BannersRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package org.sopt.lotteshopping.data.repositoryImpl
 
+import org.sopt.lotteshopping.core.util.suspendRunCatching
 import org.sopt.lotteshopping.data.mapper.toModel
 import org.sopt.lotteshopping.data.model.banners.HomeTopBannerModel
 import org.sopt.lotteshopping.data.remote.datasource.BannersRemoteDataSource
@@ -9,7 +10,7 @@ import javax.inject.Inject
 class BannersRepositoryImpl @Inject constructor(
     private val bannersDataSource: BannersRemoteDataSource
 ) : BannersRepository {
-    override suspend fun getHomeTopBanners(): Result<List<HomeTopBannerModel>> = runCatching {
+    override suspend fun getHomeTopBanners(): Result<List<HomeTopBannerModel>> = suspendRunCatching {
         bannersDataSource.getHomeTopBanners().data?.map { it.toModel() } ?: emptyList()
     }
 }
