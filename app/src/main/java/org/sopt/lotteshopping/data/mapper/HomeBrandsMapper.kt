ef1d1ec5576ea59c76f3f4b6c3ee1ccbc94eb595
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-18 17:40:17 +0900

    [chore/#32] 공백 제거

--- app/src/main/java/org/sopt/lotteshopping/data/mapper/HomeBrandsMapper.kt
+++ app/src/main/java/org/sopt/lotteshopping/data/mapper/HomeBrandsMapper.kt
@@ -3,7 +3,6 @@ package org.sopt.lotteshopping.data.mapper
 import org.sopt.lotteshopping.data.model.brands.HomeBeautyBrandModel
 import org.sopt.lotteshopping.data.remote.dto.brands.GetHomeBrandsResponse
 
-
 fun GetHomeBrandsResponse.toModel() = HomeBeautyBrandModel(
     id = id ?: 0L,
     name = name ?: "",
