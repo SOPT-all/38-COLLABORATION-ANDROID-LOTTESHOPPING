fbf3c3e7602c3413ced8f82f34647aabc58480a4
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-18 11:30:30 +0900

    [del/#27] 네트워크 통신관련 dummy 파일 정리

--- app/src/main/java/org/sopt/lotteshopping/data/remote/datasource/DummyDataSource.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.lotteshopping.data.remote.datasource
-
-import org.sopt.lotteshopping.data.remote.dto.DummyResponseDto
-
-interface DummyDataSource {
-    suspend fun getDummy(): DummyResponseDto
-}
