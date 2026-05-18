28fe1e018c75f5f59452dd0a62dad9594b8d4062
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-18 13:04:38 +0900

    [chore/#19]  BannersDataSourceModule 로네이밍 변경

--- app/src/main/java/org/sopt/lotteshopping/data/di/BannersDataSourceModule.kt
+++ app/src/main/java/org/sopt/lotteshopping/data/di/BannersDataSourceModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class DataSourceModule {
+abstract class BannersDataSourceModule {
     @Binds
     @Singleton
     abstract fun bindsBannersDataSource(
