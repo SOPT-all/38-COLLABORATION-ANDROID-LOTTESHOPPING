2d5a2404a09fcb56cd19cb6b1d46ed1d676093b0
oilbeaneda <20221444@sungshin.ac.kr>
2026-05-18 17:26:18 +0900

    [feat/#32] GetHomeBrandsResponse

---
+++ app/src/main/java/org/sopt/lotteshopping/data/remote/dto/brands/GetHomeBrandsResponse.kt
@@ -0,0 +1,15 @@
+package org.sopt.lotteshopping.data.remote.dto.brands
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetHomeBrandsResponse(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("name")
+    val name: String,
+    @SerialName("imageUrl")
+    val imageUrl: String,
+) {
+}
