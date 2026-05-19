19ea4ec3b6e78872b7d7cab5a75a42e15906cb2d
yerim0829 <123yellly@gmail.com>
2026-05-19 19:36:32 +0900

    [feat/#39] 홈 하단 배너 repository

--- app/src/main/java/org/sopt/lotteshopping/data/repository/BannersRepository.kt
+++ app/src/main/java/org/sopt/lotteshopping/data/repository/BannersRepository.kt
@@ -1,7 +1,9 @@
 package org.sopt.lotteshopping.data.repository
 
+import org.sopt.lotteshopping.data.model.banners.HomeBottomBannerModel
 import org.sopt.lotteshopping.data.model.banners.HomeTopBannerModel
 
 interface BannersRepository {
     suspend fun getHomeTopBanners(): Result<List<HomeTopBannerModel>>
+    suspend fun getHomeBottomBanner(): Result<HomeBottomBannerModel>
 }
--- app/src/main/java/org/sopt/lotteshopping/data/repositoryImpl/BannersRepositoryImpl.kt
+++ app/src/main/java/org/sopt/lotteshopping/data/repositoryImpl/BannersRepositoryImpl.kt
@@ -2,6 +2,7 @@ package org.sopt.lotteshopping.data.repositoryImpl
 
 import org.sopt.lotteshopping.core.util.suspendRunCatching
 import org.sopt.lotteshopping.data.mapper.toModel
+import org.sopt.lotteshopping.data.model.banners.HomeBottomBannerModel
 import org.sopt.lotteshopping.data.model.banners.HomeTopBannerModel
 import org.sopt.lotteshopping.data.remote.datasource.BannersRemoteDataSource
 import org.sopt.lotteshopping.data.repository.BannersRepository
@@ -10,7 +11,12 @@ import javax.inject.Inject
 class BannersRepositoryImpl @Inject constructor(
     private val bannersDataSource: BannersRemoteDataSource
 ) : BannersRepository {
+
     override suspend fun getHomeTopBanners(): Result<List<HomeTopBannerModel>> = suspendRunCatching {
         bannersDataSource.getHomeTopBanners().data?.map { it.toModel() } ?: emptyList()
+    }
+
+    override suspend fun getHomeBottomBanner(): Result<HomeBottomBannerModel> = suspendRunCatching {
+        bannersDataSource.getHomeBottomBanner().data!!.toModel()
     }
 }
