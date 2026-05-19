225a4e96111b05985e78fc7375a615f8f765891f
yerim0829 <123yellly@gmail.com>
2026-05-19 19:37:00 +0900

    [feat/#39] 홈 하단 배너 data source

--- app/src/main/java/org/sopt/lotteshopping/data/remote/datasourceImpl/BannersRemoteDataSourceImpl.kt
+++ app/src/main/java/org/sopt/lotteshopping/data/remote/datasourceImpl/BannersRemoteDataSourceImpl.kt
@@ -2,6 +2,7 @@ package org.sopt.lotteshopping.data.remote.datasourceImpl
 
 import org.sopt.lotteshopping.data.remote.datasource.BannersRemoteDataSource
 import org.sopt.lotteshopping.data.remote.dto.BaseResponse
+import org.sopt.lotteshopping.data.remote.dto.banners.GetHomeBottomBannerResponse
 import org.sopt.lotteshopping.data.remote.dto.banners.GetHomeTopBannersResponse
 import org.sopt.lotteshopping.data.remote.service.BannersService
 import javax.inject.Inject
@@ -11,4 +12,7 @@ class BannersRemoteDataSourceImpl @Inject constructor(
 ) : BannersRemoteDataSource {
     override suspend fun getHomeTopBanners(): BaseResponse<List<GetHomeTopBannersResponse>> =
         bannersService.getHomeTopBanners()
+
+    override suspend fun getHomeBottomBanner(): BaseResponse<GetHomeBottomBannerResponse> =
+        bannersService.getHomeBottomBanner()
 }
